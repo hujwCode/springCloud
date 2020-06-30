7e39cc40acca8b946cf269c756d97117c7acb94e
hujw <772809619@qq.com>
2020-06-30 11:16:38 +0800

    完成-消费端可以调用服务端的接口（通过RestTemplate）

---
+++ springCloud-consumer-dept-80/src/main/java/com/kuang/DeptConsumer_80.java
@@ -0,0 +1,16 @@
+package com.kuang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author creator hujw 2020/6/30 10:46
+ * @author updater
+ * @version 1.0.0
+ */
+@SpringBootApplication
+public class DeptConsumer_80 {
+    public static void main(String[] args) {
+        SpringApplication.run(DeptConsumer_80.class,args);
+    }
+}
--- springCloud-consumer-dept-80/src/main/java/com/kuang/springcloud/controller/DeptConsumerController.java
+++ springCloud-consumer-dept-80/src/main/java/com/kuang/springcloud/controller/DeptConsumerController.java
@@ -1,17 +1,44 @@
 package com.kuang.springcloud.controller;
 
-import org.springframework.stereotype.Controller;
+import com.kuang.springcloud.pojo.Dept;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 /**
  * @author creator hujw 2020/6/29 17:53
  * @author updater
  * @version 1.0.0
  */
-@Controller
+@RestController
 @RequestMapping("/consumer")
 public class DeptConsumerController {
 
     // 理解：消费者，不应该有service层
-    // TestTemplate 供我们直接调用就行了！ 注册到spring 中
+    // RestTemplate 供我们直接调用就行了！ 注册到spring 中
+    // (url, 实体：Map , Class<T> responseType)
+    @Autowired
+    private RestTemplate restTemplate;  //提供多种边界访问远程http服务的方法，简单的Restful服务模板~
+
+    private static final String REST_URL_PREFIX = "http://localhost:8001";
+
+    @RequestMapping("/dept/get/{id}")
+    public Dept get(@PathVariable("id") Long id) {
+        return restTemplate.getForObject(REST_URL_PREFIX + "/dept/get/" + id, Dept.class);
+    }
+
+    @RequestMapping("/dept/insert")
+    public Dept insert(Dept dept) {
+        return restTemplate.postForObject(REST_URL_PREFIX + "/dept/insert", dept, Dept.class);
+    }
+
+    @RequestMapping("/dept/list")
+    public List<Dept> list() {
+        return restTemplate.getForObject(REST_URL_PREFIX + "/dept/list", List.class);
+    }
+
 }
