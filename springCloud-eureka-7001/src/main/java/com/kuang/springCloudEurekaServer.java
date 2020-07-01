8d86a2b111fc22ec46615b8706d25e1d12f4d3f6
hujw <772809619@qq.com>
2020-07-01 17:55:52 +0800

    通过eureka实现注册服务，提供服务，消费服务

--- springCloud-eureka-7001/src/main/java/com/kuang/springCloudEurekaServer.java
+++ springCloud-eureka-7001/src/main/java/com/kuang/springCloudEurekaServer.java
@@ -2,17 +2,17 @@ package com.kuang;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
- * @author creator hujw 2020/6/30 10:46
+ * @author creator hujw 2020/7/1 16:17
  * @author updater
  * @version 1.0.0
  */
+@EnableEurekaServer
 @SpringBootApplication
-@EnableEurekaClient
-public class DeptConsumer_80 {
+public class springCloudEurekaServer {
     public static void main(String[] args) {
-        SpringApplication.run(DeptConsumer_80.class,args);
+        SpringApplication.run(springCloudEurekaServer.class,args);
     }
 }
--- springCloud-provider-dept-8001/src/main/java/com/kuang/Deptprovider_8001.java
+++ springCloud-provider-dept-8001/src/main/java/com/kuang/Deptprovider_8001.java
@@ -2,12 +2,14 @@ package com.kuang;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author creator hujw 2020/6/29 16:45
  * @author updater
  * @version 1.0.0
  */
+@EnableEurekaClient
 @SpringBootApplication
 public class Deptprovider_8001 {
     public static void main(String[] args) {
